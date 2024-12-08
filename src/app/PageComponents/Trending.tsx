55d346f4090ff8f5123c5be9c4be8425ae6a35c8
Annie <161513339+ANZIE05@users.noreply.github.com>
2024-12-09 00:16:27 +0500

    Update Trending.tsx

--- src/app/PageComponents/Trending.tsx
+++ src/app/PageComponents/Trending.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const TrendingProducts = () => {
   const products = [
@@ -42,7 +43,7 @@ const TrendingProducts = () => {
               style={{ width: "270px", height: "310px" }}
             >
               <div className="w-full h-40 flex items-center justify-center overflow-hidden rounded-lg">
-                <img
+                <Image
                   src={product.image}
                   alt={product.name}
                   className="object-contain"
