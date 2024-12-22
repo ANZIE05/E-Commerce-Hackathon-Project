7ea241224394771180afb92af37cb2ae6011b444
Annie <161513339+ANZIE05@users.noreply.github.com>
2024-12-23 01:20:00 +0500

    Update Navbar.tsx

--- src/app/Component/Navbar.tsx
+++ src/app/Component/Navbar.tsx
@@ -1,17 +1,17 @@
 "use client"
 
 import { IoIosArrowDown } from 'react-icons/io';
+import { FaTimes } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import Link from "next/link";
 import { useState } from 'react';
 
-function Navbar() {
-  const [open, setOpen] = useState(false);
-  const toggle = () => {
-    setOpen(!open);
-  };
+  const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
 
   return (
     <main className='text border-b-2 bg-neutral-100'>
@@ -26,12 +26,7 @@ function Navbar() {
         </h2>
           </div>
 
-          {/* links - animated sliding nav */}
-          <div
-            className={`${
-              open ? "translate-x-0" : "-translate-x-full"
-            }text md:flex md:translate-x-0 md:static w-[50%] md:w-auto md:bg-transparent absolute top-0 left-0 h-screen md:h-auto z-40 transition-transform duration-500 ease-in-out`}
-          >
+          <div className='flex items-center '>
             <ul className=' flex flex-col md:flex-row md:gap-x-2 lg:gap-x-5 xl:gap-x-5 2xl:gap-x-10 sm:text-md md:text-sm lg:text-md xl:text-md text-white md:text-black '>
               <li className='flex gap-1 p-4 hover:underline hover:text-pink-600 underline-offset-2'>
                 <Link href="/">Home </Link> <IoIosArrowDown className='pt-1 text-lg'/>
@@ -69,17 +64,29 @@ function Navbar() {
             </div>
 
             {/* Toggle Button for mobile */}
-            <button
-              className="text-black block md:hidden text-3xl z-50"
-              onClick={toggle}
-            >
-              ☰
-            </button>
+            <button className='block sm:hidden transition text-2xl font-bold' onClick={handleClick}>
+          {click ? <FaTimes /> : <IoMenu />}
+        </button>
           </div>
+
+             {/* Mobile Menu */}
+        {click && (
+          <div className='lg:hidden block absolute top-28 right-0 w-44 bg-slate-100 transition-transform duration-300 z-50'>
+            <ul className='text-center text-xl py-8'>
+              <Link href="/"><li className='my-4 py-2 hover:border-b-2 border-pink-600 uppercase'>Home</li></Link>
+              <Link href="/ShopLeftSidebar"><li className='my-4 py-2 hover:border-b-2 border-pink-600 uppercase'>Pages</li></Link>
+              <Link href="/GridDefault"><li className='my-4 py-2 hover:border-b-2 border-pink-600 uppercase'>Products</li></Link>
+              <Link href=""><li className='my-4 py-2 hover:border-b-2 border-pink-600 uppercase'>Blog</li></Link>
+              <Link href="/ShopList"><li className='my-4 py-2 hover:border-b-2 border-pink-600 uppercase'>Shop</li></Link>
+              <Link href="/Faq"><li className='my-4 py-2 hover:border-b-2 border-pink-600 uppercase'>Contact</li></Link>
+            </ul>
+          </div>
+        )}
 
         </div>
       </div>
     </main>
+
   );
 }
 
